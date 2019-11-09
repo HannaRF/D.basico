7c52cf1a3d7612b41b16516e8c618e567ca1f54e
Hanna <hanna.rodrigues.ismart@gmail.com>
2019-11-09 11:44:57 -0300

    100%

--- Peano Curve.py
+++ Peano Curve.py
@@ -27,13 +27,13 @@ def Peano(iterations = 5):
     draw.line([b, b, b, b + 10], fill=color1, width=1)
     draw.line([b + 10, b, b + 10, b + 10], fill=color1, width=1)
 
-    for i in range(1,iterations+1):
+    for i in range(iterations):
         image0 = concat_h(image,image)
         image1 = concat_h(image.rotate(-90),image.rotate(90))
         image = concat_v(image0,image1)
 
         draw = ImageDraw.Draw(image)
-        x =  40*i
+        x =  40*(2**i)
         draw.line([(x/2) - b,(x/2) - b,(x/2) + b,(x/2) - b],fill=color, width=1)
         draw.line([b,(x/2) - b,b,(x/2) + b], fill=color, width=1)
         draw.line([x - b,(x/2) - b,x - b,(x/2) + b], fill=color, width=1)
