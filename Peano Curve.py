257300ce309a0de11aaea30b37b6ae3a1f9f6bcd
Hanna <hanna.rodrigues.ismart@gmail.com>
2019-11-09 11:28:35 -0300

    finalizado

--- Peano Curve.py
+++ Peano Curve.py
@@ -1,26 +1,43 @@
 from PIL import Image, ImageDraw
-import numpy as np
+'''
+O número de iterações se limita a naturais menores que 10,incluindo o 0
+'''
+def concat_h(im1, im2):
+    image = Image.new('RGB', (im1.width + im2.width, im1.height))
+    image.paste(im1, (0, 0))
+    image.paste(im2, (im1.width, 0))
+    return image
 
-# rotaciona em sentido anti-horário
+def concat_v(im1, im2):
+    image = Image.new('RGB', (im1.width, im1.height + im2.height))
+    image.paste(im1, (0, 0))
+    image.paste(im2, (0, im1.height))
+    return image
 
-def rot(x,y,degree = np.pi/2):
-    x = x*np.cos(degree) - y*np.sin(degree)
-    y = x*np.sin(degree) + y*np.cos(degree)
+def Peano(iterations = 5):
+    b = 5
+    dimx = 20
+    dimy = 20
+    color1 = (0,255,255)
+    color = (255,0,255)
 
-def Peano(iterations = 1):
-    b = 10
-    dimx = 100
-    dimy = 100
+    image = Image.new("RGB", (dimx, dimy))
+    draw = ImageDraw.Draw(image)
+    draw.line([b, b, b + 10, b], fill=color1, width=1)
+    draw.line([b, b, b, b + 10], fill=color1, width=1)
+    draw.line([b + 10, b, b + 10, b + 10], fill=color1, width=1)
 
-    for i in range(iterations):
-        image = Image.new("RGB", (dimx, dimy))
+    for i in range(1,iterations+1):
+        image0 = concat_h(image,image)
+        image1 = concat_h(image.rotate(-90),image.rotate(90))
+        image = concat_v(image0,image1)
+
         draw = ImageDraw.Draw(image)
-        draw.line([b, b, b + 25, b], fill=128, width=5)
-        draw.line([b, b, b , b + 25], fill=128, width=5)
-        draw.line([b + 25, b, b + 25, b + 25], fill=128, width=5)
-        image.show()
+        x =  40*i
+        draw.line([(x/2) - b,(x/2) - b,(x/2) + b,(x/2) - b],fill=color, width=1)
+        draw.line([b,(x/2) - b,b,(x/2) + b], fill=color, width=1)
+        draw.line([x - b,(x/2) - b,x - b,(x/2) + b], fill=color, width=1)
+    image.show()
 
 
 Peano()
-
-#https://note.nkmk.me/en/python-pillow-concat-images/
